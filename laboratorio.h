f3130558eec7f6ee5b231527c34be9999bb7a79d
Eduardo Chavez <eduardo.cdorado@alumnos.udg.mx>
2020-11-14 16:37:05 -0600

    se modifico el #include computadora.h a civilizacion.h, tambien se cambiaron el nombre de la clase antigua a la nueva

--- laboratorio.h
+++ laboratorio.h
@@ -1,29 +1,29 @@
 #ifndef LABORATORIO_H
 #define LABORATORIO_H
 
-#include "computadora.h"
+#include "civilizacion.h"
 #include <vector>
 
 class Laboratorio
 {
-    vector<Computadora> computadoras;
+    vector<Civilizacion> computadoras;
 
 public:
     Laboratorio();
-    void agregarPersonaje(const Computadora &p);
+    void agregarPersonaje(const Civilizacion &p);
     void mostrar();
     void respaldar_tabla();
     void respaldar();
     void recuperar();
 
-    void insertar(const Computadora &c, size_t pos);
+    void insertar(const Civilizacion &c, size_t pos);
     size_t size();
-    void inicializar(const Computadora &c, size_t n);
+    void inicializar(const Civilizacion &c, size_t n);
     void eliminar(size_t pos);
     void ordenar();
-    Computadora* buscar(const Computadora &c);
+    Civilizacion* buscar(const Civilizacion &c);
 
-    friend Laboratorio& operator<<(Laboratorio &v, const Computadora &p)
+    friend Laboratorio& operator<<(Laboratorio &v, const Civilizacion &p)
     {
         v.agregarPersonaje(p);
 
